6c7eda831887fd7ea604fb9a203dcccae0560a49
isaacabraham <isaac.abraham@gmail.com>
2021-05-11 20:55:58 +0200

    Support spectre console

--- Program.fs
+++ Program.fs
@@ -1,7 +1,18 @@
+open Spectre.Console
+
 let items = [ 0.; 0.; 2.; 4.; 5.; 3.; 8.; 6.; 4. ]
 
 let results =
-    items |> List.windowed 3 |> List.map List.average
+    items
+    |> List.windowed 3
+    |> List.map (fun numbers -> Seq.last numbers, List.average numbers)
 
-for result in results do
-    System.Console.WriteLine $"The next value is {result}"
+let table =
+    Table()
+        .AddColumns("Value", "Simple Moving Average")
+
+for value, average in results do
+    table.AddRow(value.ToString(), average.ToString "F2")
+    |> ignore
+
+AnsiConsole.Render table
